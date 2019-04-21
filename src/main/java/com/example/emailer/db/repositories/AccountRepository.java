c2e90488b82c58bb20496f9d6fc2ba5a0e21442c
cosmoskey <barskoidim@gmail.com>
2019-04-21 20:22:42 +0300

    Add method to lookup by name

--- src/main/java/com/example/emailer/db/repositories/AccountRepository.java
+++ src/main/java/com/example/emailer/db/repositories/AccountRepository.java
@@ -3,8 +3,11 @@ package com.example.emailer.db.repositories;
 import com.example.emailer.db.entities.Account;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AccountRepository extends Neo4jRepository<Account, Long> {
     Optional<Account> findByEmail(String email);
+
+    List<Account> findAllByFirstNameAndLastName(String firstName, String lastName);
 }
