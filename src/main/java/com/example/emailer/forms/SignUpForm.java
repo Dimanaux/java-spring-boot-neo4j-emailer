a680ee6f49be27178eb4e1c41523a235121d7757
cosmoskey <barskoidim@gmail.com>
2019-04-23 10:09:32 +0300

    Get rid of lombok

--- src/main/java/com/example/emailer/forms/SignUpForm.java
+++ src/main/java/com/example/emailer/forms/SignUpForm.java
@@ -1,13 +1,47 @@
 package com.example.emailer.forms;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
 public class SignUpForm {
     private String email;
     private String firstName;
     private String lastName;
     private String password;
+
+    public SignUpForm() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public SignUpForm setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public SignUpForm setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public SignUpForm setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public SignUpForm setPassword(String password) {
+        this.password = password;
+        return this;
+    }
 }
