9ec082d5bd6eab21b20136740140d7fb5a5ca500
cosmoskey <barskoidim@gmail.com>
2019-05-02 19:50:58 +0300

    Add more info to messages from db

--- src/main/java/com/example/emailer/db/repositories/MessageRepository.java
+++ src/main/java/com/example/emailer/db/repositories/MessageRepository.java
@@ -9,14 +9,14 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface MessageRepository extends Neo4jRepository<Message, Long> {
-    @Query("MATCH (a:Account {email: {account_email} })-->(m: Message) RETURN m;")
+    @Query("MATCH (sender:Account {email: {account_email} })-[by]-(m: Message)-[to:SENT_TO]->(r:Account) RETURN sender, m, by, to, r;")
     List<Message> findAllAvailableToAccount(@Param("account_email") String accountEmail);
 
     List<Message> findAllBySender(Account account);
 
-    @Query("MATCH (a: Account)-[:SENT]->(m: Message {status: 'DRAFT'}) RETURN m;")
+    @Query("MATCH (a: Account)-[r]->(m: Message {status: 'DRAFT'}) RETURN m, r, a;")
     List<Message> findAllDrafts(Account account);
 
-    @Query("MATCH (a: Account)-[:RECEIVED]->(m: Message) RETURN m;")
+    @Query("MATCH (a: Account)-[r:RECEIVED]->(m: Message) RETURN m, r, a;")
     List<Message> findAllReceivedByAccount(Account account);
 }
