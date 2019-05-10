37b8574665d3920845882041637a6b65dae24107
cosmoskey <barskoidim@gmail.com>
2019-05-10 09:17:35 +0300

    Rename from MessageSender (generify)

--- src/main/java/com/example/emailer/services/functions/Creator.java
+++ src/main/java/com/example/emailer/services/functions/Creator.java
@@ -3,6 +3,6 @@ package com.example.emailer.services.functions;
 import com.example.emailer.db.entities.Account;
 
 @FunctionalInterface
-public interface MessageSender {
+public interface Creator {
     void by(Account account);
 }
