92b638452e7d0e3682dd0c22bda2f9e2b8b4be15
cosmoskey <barskoidim@gmail.com>
2019-05-17 14:19:24 +0300

    delete AuthAspect, add ModelMap aspect, make it work

---
+++ src/main/java/com/example/emailer/aspects/ModelMapAspect.java
@@ -0,0 +1,41 @@
+package com.example.emailer.aspects;
+
+import com.example.emailer.security.AccountDetails;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ui.ModelMap;
+
+@Aspect
+@Configuration
+public class ModelMapAspect {
+    @Pointcut(value = "execution(* com.example.emailer.controllers..*(..))")
+    public void getPage() {
+    }
+
+    @Around("getPage() && args(accountDetails,modelMap,..)")
+    public Object putFolders(ProceedingJoinPoint pjp,
+                             AccountDetails accountDetails,
+                             ModelMap modelMap) throws Throwable {
+        modelMap.put("folders", accountDetails.getAccount().getFolders());
+        return pjp.proceed(pjp.getArgs());
+    }
+
+    @Around("getPage() && args(accountDetails,modelMap,..)")
+    public Object putCurrentUser(ProceedingJoinPoint pjp,
+                                 ModelMap modelMap,
+                                 AccountDetails accountDetails) throws Throwable {
+        modelMap.put("current_user", accountDetails.getAccount());
+        return pjp.proceed(pjp.getArgs());
+    }
+
+    @Around("getPage() && args(accountDetails,modelMap,..)")
+    public Object putGroups(ProceedingJoinPoint pjp,
+                                 ModelMap modelMap,
+                                 AccountDetails accountDetails) throws Throwable {
+        modelMap.put("groups", accountDetails.getAccount().getGroups());
+        return pjp.proceed(pjp.getArgs());
+    }
+}
