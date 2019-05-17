264428644356bb92720125185acd396514feeb9c
cosmoskey <barskoidim@gmail.com>
2019-05-17 17:17:05 +0300

    Add methods to group repository: bind, setOwner, findGroups

--- src/main/java/com/example/emailer/db/repositories/GroupRepository.java
+++ src/main/java/com/example/emailer/db/repositories/GroupRepository.java
@@ -1,7 +1,19 @@
 package com.example.emailer.db.repositories;
 
 import com.example.emailer.db.entities.Group;
+import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface GroupRepository extends Neo4jRepository<Group, Long> {
+    @Query("MATCH (g:Group {groupId: {groupId}}) MATCH (a:Account {accountId: {accountId}}) CREATE (g)-[:OWNED_BY]->(a);")
+    void setOwner(@Param("groupId") Long groupId, @Param("accountId") Long accountId);
+
+    @Query("MATCH (g:Group {groupId: {groupId}}) MATCH (a:Account {accountId: {accountId}}) CREATE (g)-[:CONTAINS]->(a) CREATE (a)-[:IN]->(g);")
+    void bindAccountToGroup(@Param("groupId") Long groupId, @Param("accountId") Long accountId);
+
+    @Query("MATCH (a:Account {accountId: {accountId}})-[:IN]-(g:Group)-[c:CONTAINS]-(p) RETURN a, g, c, p;")
+    List<Group> groupsOf(@Param("accountId") Long accountId);
 }
